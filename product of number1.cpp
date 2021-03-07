678be78ac0df62d3fd3556b2cbe2e749aafc694f
ChhaviShree <80169229+ChhaviShree@users.noreply.github.com>
2021-03-07 21:06:43 +0530

    product of two number

---
+++ product of number1.cpp
@@ -0,0 +1,11 @@
+// find the product of two numbers entered by the User
+#include<iostream>
+using namespace std;
+ main()
+{
+    float n1,n2,product;
+    cout <<"Enter the two number"<<"\n";
+    cin >>n1>>n2;
+    product= n1*n2;
+    cout <<"product = "<<product;
+}
