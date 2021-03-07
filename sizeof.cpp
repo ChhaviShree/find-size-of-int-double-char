cfbad216c88f79fe64b83fd187e2a29128f82e52
ChhaviShree <80169229+ChhaviShree@users.noreply.github.com>
2021-03-07 15:44:15 +0530

    created sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,14 @@
+// C++ Program to Find Size of int, float, double and char
+#include<iostream>
+using namespace std;
+main()
+{
+    int a;
+    float b;
+    double d;
+    char c;
+    cout<<"size of int is "<<sizeof(a)<<"\n";
+    cout<<"size of float is  "<<sizeof(b)<<"\n";
+    cout<<"size of double is "<<sizeof(d)<<"\n";
+    cout<<"size of character is  "<<sizeof(c)<<"\n";
+}
